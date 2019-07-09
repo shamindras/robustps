427a87861ab8e4b8a18b2f05a0de1876299154ba
ispnp <>
2019-07-09 17:05:06 -0400

    Delete data generation function.
    This function is ported to utils-data-generation.R

--- analysis/example-modular-conformal.R
+++ analysis/example-modular-conformal.R
@@ -82,49 +82,6 @@ gen_density_samples_nd <- function(density_list, n_samples){
     base::return(out)
 }
 
-#' Generate specified number of samples from the required Huber density function
-#' NOTE: we are generating samples for, f, g separately and then combining into
-#' an h sample.
-#' TODO: Consider creating h sample directly given f, g, epsilon
-#'
-#' @param density_p0
-#' @param density_q
-#' @param eps
-#' @param n_samples
-#'
-#' @return
-#' @export
-gen_huber_samples_nd <- function(density_p0 = list(type = "normal",
-                                                   mu_vec = 0, sigma = 1),
-                                 density_q = list(type = "normal",
-                                                  mu_vec = 5, sigma = 1),
-                                 eps,
-                                 n_samples){
-
-    components <- sample(1:2, prob = c(1-eps, eps),
-                         size = n_samples, replace = TRUE)
-
-    n_samples_p0 <- sum(components == 1)
-    n_samples_q <- n_samples - n_samples_p0
-
-    samples_p0 <- gen_density_samples_nd(density_list = density_p0,
-                                         n_samples = n_samples_p0)
-    samples_q <- gen_density_samples_nd(density_list = density_q,
-                                        n_samples = n_samples_q)
-
-    # Convert to a single tibble
-    samples_p0_df <- samples_p0 %>%
-                        tibble::as_tibble(x = .) %>%
-                        dplyr::mutate(.data = ., compts = 1)
-
-    samples_q_df <- samples_q %>%
-                        tibble::as_tibble(x = .) %>%
-                        dplyr::mutate(.data = ., compts = 2)
-
-    samples_huber <- dplyr::bind_rows(samples_p0_df, samples_q_df)
-
-    base::return(samples_huber)
-}
 
 # hist(gen_huber_samples_nd(density_p0 = list(type = "normal", mu_vec = 0, sigma = 1),
 #                      density_q = list(type = "normal", mu_vec = 5, sigma = 1),
