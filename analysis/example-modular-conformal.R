5a5d4d091ebcf5310daa60b8059ec94ed6b93fa6
Shamindra Shrotriya <shamindra.shrotriya@gmail.com>
2019-07-19 13:19:32 -0400

    ENH: Update modular conformal code for mean (non-robust) and add skeleton for new code

--- analysis/example-modular-conformal.R
+++ analysis/example-modular-conformal.R
@@ -3,18 +3,24 @@
 # methodology. The final goal is to estimate robust prediction sets with
 # coverage and lebesgue measure guarantees
 
+# Clean up ---------------------------------------------------------------------
+rm(list = ls())
+cat("\014")
+
 # Setup ------------------------------------------------------------------------
 # TODO: Delete this and move to package dependencies
 # The xfun package needs to be separately installed (if not already on your
 # system). So simply run the following line of code:
 # install.packages(c("xfun", "here", "MASS", "tidyverse", "conflicted"))
-pckgs <- c("here", "MASS", "tidyverse", "conflicted")
+pckgs <- c("here", "MASS", "tidyverse", "conflicted", "devtools")
 xfun::pkg_attach2(pckgs)
 
 # Source GLOBAL files ----------------------------------------------------------
 # We can source all R files from our core functions
 fs::dir_ls(here::here("R"), glob = "*.R") %>%
     purrr::walk(.x = ., .f = ~base::source(file = .x))
+
+# devtools::install_local(path = here::here())
 
 # Define GLOBAL functions ------------------------------------------------------
 
@@ -46,27 +52,78 @@ diff_vec <- function(inp_vec, diff_val){
     base::return(inp_vec - diff_val)
 }
 
-# Generate Contaminated Multivariate normal Data
-#' Generate specified number of samples from the required density function
+#' Takes row-by-row differences from a common vector of same length
+#' as the number of columns in the original dataframe. Typical use case
+#' is to take differences from a mean from each observation (i.e. row of
+#' numeric values in the datframe). This will only work if the dataframe is
+#' contains numeric values
 #'
-#' @param density_list
-#' @param n_samples
+#' @param inp_df (data.frame) : A dataframe of numeric values
+#' @param anchor_vec (vector) : A vector of numeric values of the same length
+#' as the number of columns as \code{inp_df}
 #'
-#' @return
 #' @export
-gen_density_samples_nd <- function(density_list, n_samples){
+get_mean_diff_vec <- function(inp_df, anchor_vec){
+    out_vec <- inp_df %>%
+        purrr::transpose(.l = .) %>%
+        purrr::map(.x = .,
+                   .f = function(x){dvec <-  x - anchor_vec
+                   dvec <- base::unname(obj = dvec)
+                   base::return(x - anchor_vec)})
+    base::return(out_vec)
+}
 
-    density_type <- density_list$type
 
-    if(density_type == "normal"){
-        out <- MASS::mvrnorm(n = n_samples,
-                             mu = density_list$mu_vec,
-                             Sigma = density_list$sigma)
-    } else {
-        stop("We only accept normal as our density type currently")
-    }
+#' Given a list of numeric vectors return a single numeric vector which contains
+#' the specified norm of each of the numeric vector. This ouput numeric vector
+#' will have the same length as the original input list of numeric vectors.
+#'
+#' @param mean_diff_vec (list) : A list of numeric vectors. They should be
+#' of the same length
+#' @param norm_type (character) : The specified norm type to take of each
+#' numeric vector i.e. "2" calculates the 2-norm, "1" calculates the 1-norm and
+#' "I" calculates the max-norm. Refer to \code{base::norm} for more details
+#' on norm types.
+#'
+#' @export
+get_mean_diff_vec_norms <- function(mean_diff_vec, norm_type = "2"){
+    out_vec <- mean_diff_vec %>%
+                purrr::map_dbl(.x = ., .f = ~base::norm(x = .x,
+                                                        type = norm_type))
 
-    base::return(out)
+    base::return(out_vec)
+}
+
+#' Generates a 2 column data frame containing a cartesian product of a
+#' sequence of x-values and y-values to form a rectangular grid of 2D pairs
+#'
+#' @param grid_x_by (double) : The length of the grid in the "x" variable
+#' @param grid_y_by (double) : The length of the grid in the "y" variable
+#' @param grid_x_min (double) : The minimum x-value for the grid
+#' @param grid_x_max (double) : The maximum x-value for the grid
+#' @param grid_y_min (double) : The minimum y-value for the grid
+#' @param grid_y_max (double) : The maximum y-value for the grid
+#' @param var1_nm (character) : The name of the first column, default is "x1"
+#' @param var2_nm (character) : The name of the second column, default is "x2"
+#'
+#' @export
+generate_2d_grid <- function(grid_x_by, grid_y_by,
+                             grid_x_min, grid_x_max,
+                             grid_y_min, grid_y_max,
+                             var1_nm = "x1",
+                             var2_nm = "x2"){
+    # Grid sequence by axis
+    grid_x_seq <- base::seq(from = grid_x_min, to = grid_x_max,
+                            by = grid_x_by) %>%
+        tibble::enframe(x = ., name = NULL, value = var1_nm)
+    grid_y_seq <- base::seq(from = grid_y_min, to = grid_y_max,
+                            by = grid_y_by) %>%
+        tibble::enframe(x = ., name = NULL, value = var2_nm)
+
+    # Generate the grid
+    plt_grid <- grid_x_seq %>% tidyr::crossing(grid_y_seq)
+
+    base::return(plt_grid)
 }
 
 
@@ -85,63 +142,144 @@ base::set.seed(seed = SEED_VAL)
 EPSILON <- 0.1
 ALPHA <- 0.05
 
-# Generate bivariate normal (BVN) data -----------------------------------------
-
+# DEFINE Global variables ------------------------------------------------------
 # Core BVN parameters
-NUM_OBS <- 1000
+NUM_OBS <- 500
 BIN_SPLIT_PROP <- 0.5
 
-# Covariance matrix
+# General useful variables
 I <- base::diag(x = c(1, 1)) # identity matrix
+
+# TRUE values
+# mean vector
+TRUE_MU_VEC <- c(0, 0)
+# Covariance matrix
 TRUE_SIGMA_MAT <- 2*I
 
 # Contmination
+# mean vector
+CONT_MU_VEC <- c(2, 2)
 # CONT_SIGMA_MAT <- base::matrix(c(2,0,0,2), 2, 2)
 CONT_SIGMA_MAT <- 0.5*I
 
-# mean vector
-mu_vec <- c(0, 0)
+# Default column names
+BVN_NVARS <- 2
+BVN_COLNAMES <- base::seq.int(from = 1, to = BVN_NVARS, by = 1) %>%
+                    purrr::map_chr(.x = .,
+                                   .f = ~stringr::str_c("x", .x, sep = ""))
+
+# TYPE I, TYPE_II error vars
+NUM_OBS_TYPE_I_ERR <- 1000
+NUM_OBS_TYPE_II_ERR <- NUM_OBS_TYPE_I_ERR
+
+# Generate UNCONTAMINATED bivariate normal (BVN) data --------------------------
 
 # Generate the required multivariate normal data
-bvn_sim_mat <- MASS::mvrnorm(n = NUM_OBS, mu = mu_vec, Sigma = I)
+bvn_sim_mat <- MASS::mvrnorm(n = NUM_OBS, mu = TRUE_MU_VEC,
+                             Sigma = TRUE_SIGMA_MAT)
 
 # Keep it in data frame format for convenience
-bvn_sim_df <- bvn_sim_mat %>% tibble::as_tibble(x = .)
+bvn_sim_df <- bvn_sim_mat %>%
+                tibble::as_tibble(x = .) %>%
+                magrittr::set_colnames(x = ., value = BVN_COLNAMES)
 
 # Plot density plots on the sample data points
 bvn_sim_df %>%
-    ggplot2::ggplot(data = ., aes(x = V1, y = V2)) +
+    ggplot2::ggplot(data = ., aes(x = x1, y = x2)) +
     ggplot2::geom_point() +
     ggplot2::geom_density_2d()
+
+# Generate CONTAMINATED bivariate normal (BVN) data ----------------------------
 
 # Plot the contours empirically for the eps-contaminated bivariate normal
 bvn_sim_cont_df <- gen_huber_samples_nd(density_p0 =
                                              list(type = "normal",
-                                                  mu_vec = c(0, 0),
+                                                  mu_vec = TRUE_MU_VEC,
                                                   sigma = TRUE_SIGMA_MAT),
                                          density_q =
                                              list(type = "normal",
-                                                  mu_vec = c(3, 3),
+                                                  mu_vec = CONT_MU_VEC,
                                                   sigma = CONT_SIGMA_MAT),
                                          eps = EPSILON,
                                          n_samples = NUM_OBS) %>%
+                        magrittr::set_colnames(x = .,
+                                               value = c(BVN_COLNAMES, "compts")) %>%
                         dplyr::mutate(.data = ., compts =
                                           forcats::as_factor(compts))
 
+# Plot density plots on the sample data points
 bvn_sim_cont_df %>%
-    ggplot2::ggplot(data = ., aes(x = V1, y = V2, col = compts)) +
+    ggplot2::ggplot(data = ., aes(x = x1, y = x2, col = compts)) +
     ggplot2::geom_point() +
     ggplot2::geom_density_2d()
 
-# Perform sample splitting of the input data -----------------------------------
+
+# TODO: Insert sample-splitting code here
+
+# SCORE Function - distance to mean
+# Get the sample mean vector
+get_means <- bvn_sim_df %>% smry_df_features(inp_df = ., smry_type = mean)
+
+# Compute the distance measure on the second sample ----------------------------
+mean_diff_vec <- get_mean_diff_vec(inp_df = bvn_sim_df, anchor_vec = get_means)
+
+# Get the 2-norm difference for the mean vector i.e. the phi function
+mean_diff_vec_norms <- get_mean_diff_vec_norms(mean_diff_vec = mean_diff_vec,
+                                               norm_type = "2")
+
+# Compute the appropriate quantile threshold -----------------------------------
+des_covg_prob <- (1 - ALPHA)
+new_thresh_qtil <- mean_diff_vec_norms %>%
+                    stats::quantile(x = .,
+                                    probs = c(des_covg_prob),
+                                    na.rm = FALSE,
+                                    names = FALSE,
+                                    type = 3) # This is to get the order
+                                              # statistic, no interpolation
+
+# Apply the threshold and retrieve the confidence set  -------------------------
+# Generate the grid
+plt_grid <- generate_2d_grid(grid_x_by = 0.2, grid_y_by = 0.2,
+                             grid_x_min = -8, grid_x_max = 8,
+                             grid_y_min = -8, grid_y_max = 8,
+                             var1_nm = "x1",
+                             var2_nm = "x2")
+
+mean_diff_vec_grid <- get_mean_diff_vec(inp_df = plt_grid,
+                                        anchor_vec = get_means)
+
+mean_diff_vec_norms_grid <- get_mean_diff_vec_norms(
+                                mean_diff_vec = mean_diff_vec_grid,
+                                norm_type = "2")
+
+conf_set_grid <- mean_diff_vec_norms_grid[mean_diff_vec_norms_grid <=
+                                              new_thresh_qtil]
+length(conf_set_grid)
+
+plt_grid_final <- plt_grid %>%
+                    dplyr::mutate(dist_to_mean = mean_diff_vec_norms_grid,
+                                  ind_conf_set =
+                                      as.factor(dist_to_mean <= new_thresh_qtil))
+bvn_sim_df %>%
+    ggplot2::ggplot(data = ., aes(x = x1, y = x2)) +
+    ggplot2::geom_point() +
+    geom_point(data = plt_grid_final, aes(colour = ind_conf_set))
+
+# TYPE I error
+# Simulate from the true distribution
+# i.e. set epsilon to 0
+bvn_sim_typeI <- MASS::mvrnorm(n = NUM_OBS_TYPE_I_ERR, mu = TRUE_MU_VEC,
+                               Sigma = TRUE_SIGMA_MAT)
+
+# SAMPLE SPLITTING of the input data -------------------------------------------
 # We perform a binary sample split of the input dataframe
 bin_split_prop <- BIN_SPLIT_PROP
 bin_split_props <- c(bin_split_prop, 1 - bin_split_prop)
 n_obs <- base::nrow(x = bvn_sim_df)
 
 # Get the split allocation for each component
-compts <- base::sample(x = c(1:2), prob = bin_split_props, size = n_obs,
-                       replace = TRUE)
+compts <- base::sample(x = c(1:2), prob = bin_split_props,
+                       size = n_obs, replace = TRUE)
 
 # Randomly split the samples in 2 separate dataframes
 split_smpls <- bvn_sim_df %>%
@@ -153,51 +291,3 @@ split_smpls <- bvn_sim_df %>%
 bvn_sim_samp1_df <- split_smpls[[1]]
 bvn_sim_samp2_df <- split_smpls[[2]]
 
-# Get the sample mean vector
-get_means <- bvn_sim_df %>% smry_df_features(inp_df = ., smry_type = mean)
-
-# Compute the distance measure on the second sample ----------------------------
-mean_diff_vec <- bvn_sim_df %>%
-                    purrr::map2_dfc(.x = ., .y = get_means, ~ .x - .y) %>%
-                    purrr::pmap(., c, use.names = FALSE)
-mean_diff_vec
-
-# Get the 2-norm difference for the mean vector i.e. the phi function
-mean_diff_vec_norms <- mean_diff_vec %>%
-                        purrr::map_dbl(.x = .,
-                                       .f = ~base::norm(x = .x, type = "2"))
-
-# Compute the appropriate quantile threshold -----------------------------------
-new_thresh_qtil <- (1 - ALPHA)
-new_thresh_qtil <- mean_diff_vec_norms %>%
-                    stats::quantile(x = .,
-                                    probs = c(1 - new_thresh_qtil),
-                                    na.rm = FALSE,
-                                    names = FALSE)
-
-# Apply the threshold and retrieve the confidence set  -------------------------
-conf_set <- mean_diff_vec_norms[mean_diff_vec_norms >= new_thresh_qtil]
-
-# DO this on the grid
-# Grid x sequence
-grid_x_by <- 0.2
-grid_y_by <- 0.2
-
-# Grid end-points (x, y)
-grid_x_min <- -5
-grid_x_max <- 5
-grid_y_min <- -5
-grid_y_max <- 5
-
-# Grid sequence by axis
-grid_x_seq <- base::seq(from = grid_x_min, to = grid_x_max, by = grid_x_by) %>%
-                tibble::enframe(x = ., name = NULL, value = "x")
-grid_y_seq <- base::seq(from = grid_y_min, to = grid_y_max, by = grid_y_by) %>%
-                tibble::enframe(x = ., name = NULL, value = "y")
-
-# Generate the grid
-plt_grid <- grid_x_seq %>% tidyr::crossing(grid_y_seq)
-
-# Apply the threshold to the validation set
-# Not quite sure how we want to do this here specifically. We can discuss
-# and finalize this
---
+++ analysis/skeleton-modular-conformal.R
@@ -0,0 +1,29 @@
+# 2 Non-robust prediction sets -------------------------------------------------
+
+## 2.1 work with regular calibration for uncontaminted data
+
+## 2.2 typically fail to provide the right coverage under data contamination
+
+## 2.3 in the worst case could have severe coverage failure
+
+# 3 Naive coverage expansion robust prediction sets ----------------------------
+
+## 3.1 provides the right coverage under contamination
+
+## 3.2 but can be arbitrarily bad in terms of size in the worst case
+
+# 4 Projection (on true class) + expansion robust prediction sets --------------
+
+## 4.1 also provides the right coverage under contamination
+
+## 4.2 in addition, have better sizes compared to naive expansion sets
+
+## 4.3 but projection step is computationally infeasible in general
+
+# 5 Practical projection via outlier removal + coverage expansion --------------
+
+## 5.1 provides the right coverage under structured contamination
+
+## 5.2 and have good sizes compared to true projection + expansion sets
+
+## 5.3 different outlier removal depending on different structural assumptions
